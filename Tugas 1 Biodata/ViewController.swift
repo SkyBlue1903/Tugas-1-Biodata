59866d40035742fde69ef7ca6008ed021258307d
Erlangga Anugrah Arifin <angga1433@gmail.com>
2022-10-11 08:56:47 +0700

    Initial Commit

---
+++ Tugas 1 Biodata/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tugas 1 Biodata
+//
+//  Created by Erlangga Anugrah Arifin on 11/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
